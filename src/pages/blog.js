f15833687459385724e4273bf087cadbdada88b6
Matthew Pagan <mastapegs01@gmail.com>
2020-05-17 12:24:18 -0400

    Refactor blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -24,11 +24,21 @@ const Blog = ({ data }) => {
   return (
     <>
       <h1>Blog</h1>
+      <p>
+        Several posts showing our family's quarentine shenanigans.
+      </p>
+      <h2>Posts</h2>
       <ul>
         {data.allMarkdownRemark.edges.map(edge => (
           <li key={edge.node.id} className={styles.post}>
             <p>
-              {`${edge.node.frontmatter.title} - ${edge.node.frontmatter.date}`}
+              <span>
+                <strong>{edge.node.frontmatter.title}</strong>
+              </span>
+              {' - '}
+              <span>
+                {edge.node.frontmatter.date}
+              </span>
             </p>
             <p>
               {edge.node.excerpt}
